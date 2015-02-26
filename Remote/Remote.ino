10a539bd4d16ffc50099824944f4a41d939f2c51
Matthew Pateman <matthewpateman@me.com>
2015-02-26 21:42:17 +0000

    Added a Pause Button

--- Remote/Remote.ino
+++ Remote/Remote.ino
@@ -4,20 +4,12 @@
 
 using namespace RemoteSpotify;
 
-// pin 2 on the RGB shield is the red led
-// pin 3 on the RGB shield is the green led
-// pin 4 on the RGB shield is the blue led
-MultiColorLed led(2, 3, 4);
-
-// define colors for use with the led
-Color red(10, 0, 0);
-Color green(0, 10, 0);
-Color blue(0, 0, 100);
-
-// pin 5 on the RGB shield is button 1
-Button nextBtn(5);
-// pin 6 on the RGB shield is button 2
-Button prevBtn(6);
+// pin 2 on the RFDuino is used for the Next Button
+Button nextBtn(2);
+// pin 3 on the RFDuino is used for the Pause Button
+Button pauseBtn(3);
+// pin 4 on the RFDuino is used for the Prev Button
+Button prevBtn(4);
 
 void setup() {
   Serial.begin(9600);
@@ -37,9 +29,10 @@ void setup() {
   Serial.println("Configuring pinWake callbacks");
   RFduino_pinWakeCallback(nextBtn.GetPin(), HIGH, nextBtnPressed);
   RFduino_pinWakeCallback(prevBtn.GetPin(), HIGH, prevBtnPressed);
+  RFduino_pinWakeCallback(pauseBtn.GetPin(), HIGH, pauseBtnPressed);
   
   // turn on red led to indicate no connection
-  led.TurnOn(red);
+ // led.TurnOn(red);
 }
 
 int nextBtnPressed(uint32_t ulPin)
@@ -49,7 +42,7 @@ int nextBtnPressed(uint32_t ulPin)
   {
     Serial.println("Sending nextBtn signal");
     RFduinoBLE.send(0);
-    led.BlinkAndRestoreState(300, 0, 1, blue);
+   // led.BlinkAndRestoreState(300, 0, 1, blue);
     return 1;
   }
   
@@ -63,7 +56,21 @@ int prevBtnPressed(uint32_t ulPin)
   {
     Serial.println("Sending prevBtn signal");
     RFduinoBLE.send(1);
-    led.BlinkAndRestoreState(300, 0, 1, blue);
+    //led.BlinkAndRestoreState(300, 0, 1, blue);
+    return 1;
+  }
+  
+  return 0;  // don't exit RFduino_ULPDelay
+}
+
+int pauseBtnPressed(uint32_t ulPin)
+{
+  Serial.println("Entered prevBtn pinwake callback");
+  if (pauseBtn.Debounce(HIGH))
+  {
+    Serial.println("Sending pauseBtn signal");
+    RFduinoBLE.send(2);
+    //led.BlinkAndRestoreState(300, 0, 1, blue);
     return 1;
   }
   
@@ -84,23 +91,23 @@ void loop() {
   }
   
   // if any button has been down for over 3 seconds, shut down
-  if (nextBtn.GetHoldDuration() > 3000 || prevBtn.GetHoldDuration() > 3000)
+  if (nextBtn.GetHoldDuration() > 3000 || pauseBtn.GetHoldDuration() > 3000 || prevBtn.GetHoldDuration() > 3000)
       shutdown();
 }
 
 void RFduinoBLE_onConnect() {
-  led.TurnOn(green);
+ // led.TurnOn(green);
 }
 
 void RFduinoBLE_onDisconnect()
 {
-  led.TurnOn(red);
+ // led.TurnOn(red);
 }
 
 void shutdown()
 {
   // blink the red led to indicate shutdown
-  led.Blink(200, 200, 5, red);
+  // led.Blink(200, 200, 5, red);
   
   // this is so that the button release doesn't wake us up again
   delay(3000);
